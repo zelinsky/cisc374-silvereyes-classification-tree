52716e93852aa01a91d495c111ef2f49895f17f7
Joel Turk <jturk@udel.edu>
2019-01-16 09:40:38 +1300

    Full commit

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Bird : MonoBehaviour
+{
+    public string color;
+    public float height;
+    public bool flight;
+
+    private string species;
+    private string[] colors = { "green", "brown" };
+    private string[] speciess = { "kiwi", "Moa" };
+    // Start is called before the first frame update
+    void Start()
+    {
+        color = (Random.value > 0.5f) ? colors[0] : colors[1];
+        height = Random.value * 2f;
+        flight = Random.value > 0.5f;
+        if (height > 1f)
+        {
+            species = speciess[1];
+        }
+        else
+        {
+            species = speciess[0];
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController instance;
+    public int birdPoolSize = 5;
+    public GameObject birdPrefab;
+    public Button button1, button2, button3;
+
+    private GameObject[] birds;
+    private Vector2 objectPoolPosition = new Vector2(0, 5);
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        birds = new GameObject[birdPoolSize];
+        for (int i = 0; i < birdPoolSize; i++)
+        {
+            birds[i] = (GameObject)Instantiate(birdPrefab, objectPoolPosition, Quaternion.identity);
+        }
+    }
+
+    void Start()
+    {
+        //Calls the TaskOnClick/TaskWithParameters/ButtonClicked method when you click the Button
+        button1.onClick.AddListener(OnClickColor);
+        button2.onClick.AddListener(OnClickHeight);
+        button3.onClick.AddListener(OnClickFlight);
+    }
+
+    void OnClickHeight()
+    {
+        //Output this to console when Button1 or Button3 is clicked
+        foreach(GameObject b in birds)
+        {
+            Bird bird; 
+            bird = b.GetComponent<Bird>();
+            if (bird.height > 1)
+                bird.gameObject.layer = 8;
+            else
+                bird.gameObject.layer = 9;
+        }
+    }
+
+    void OnClickColor()
+    {
+        //Output this to console when Button1 or Button3 is clicked
+        foreach (GameObject b in birds)
+        {
+            Bird bird;
+            bird = b.GetComponent<Bird>();
+            if (bird.color.Equals("brown"))
+                bird.gameObject.layer = 8;
+            else
+                bird.gameObject.layer = 9;
+        }
+    }
+    void OnClickFlight()
+    {
+        //Output this to console when Button1 or Button3 is clicked
+        foreach (GameObject b in birds)
+        {
+            Bird bird;
+            bird = b.GetComponent<Bird>();
+            if (bird.flight)
+                bird.gameObject.layer = 8;
+            else
+                bird.gameObject.layer = 9;
+        }
+    }
+
+    void TaskWithParameters(string message)
+    {
+        //Output this to console when the Button2 is clicked
+        Debug.Log(message);
+    }
+
+    void ButtonClicked(int buttonNo)
+    {
+        //Output this to console when the Button3 is clicked
+        Debug.Log("Button clicked = " + buttonNo);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
