5d5c590bf53f92e06306a402c9e862cabeb00ad7
Joel Turk <jturk@udel.edu>
2019-01-17 08:55:05 +1300

    Put BirdHeroblue into new Resources folder, bird.cs into scripts folder

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -13,9 +13,16 @@ public class Bird : MonoBehaviour
     private string species;
     private string[] colors = { "green", "brown" };
     private string[] speciess = { "kiwi", "Moa" };
+
+    private Sprite spriteB;
+    private Sprite spriteY;
+    Vector2 scaleB = new Vector2(1, 1);
+    Vector2 scaleS = new Vector2(0.5f, 0.5f);
     // Start is called before the first frame update
     void Start()
     {
+        spriteB = Resources.Load<Sprite>("BirdHeroblue");
+        spriteY = Resources.Load<Sprite>("BirdHero");
         color = (Random.value > 0.5f) ? colors[0] : colors[1];
         height = Random.value * 2f;
         flight = Random.value > 0.5f;
@@ -26,6 +33,26 @@ public class Bird : MonoBehaviour
         else
         {
             species = speciess[0];
+        }
+        // sprite editing
+        // color
+        if (color == "green")
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = spriteB;
+        }
+        else if (color == "brown")
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = spriteY;
+        }
+        //size
+        //gameObject.transform.Translate(1.0f, 1.0f, 1.0f);
+        if (height >= 1)
+        {
+            gameObject.transform.localScale = scaleB;
+        }
+        else if (height < 1)
+        {
+            gameObject.transform.localScale = scaleS;
         }
     }
 
